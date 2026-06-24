f60cab62ac0ab44e2aac494e24dc90a912bd9933
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-06-24 17:01:20 +0900

    演習3-2

---
+++ components/chapter03/ex3-1/WelcomeMessage.tsx
@@ -0,0 +1,23 @@
+/**
+ * 演習 3-1 WelcomeMessageコンポーネントを作成する
+ */
+export default function WelcomeMessage() {
+    return (
+        <div>
+            <h1>ようこそ！</h1>
+            <p>初めてのReactコンポーネントです。</p>
+            <p>確認</p><br />
+            <p>テスト</p>
+        </div>
+    );
+}
+export function Message() {
+    return (
+        <div>
+            <h1>kakuninn</h1>
+            <p>tesuto</p>
+
+        </div>
+
+    );
+}
